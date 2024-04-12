4453c1886e3d78e9db7d53b9912db5e61b0e5b16
Aleksandr Zhigulin <66275482+aleksandrzhigulin@users.noreply.github.com>
2024-04-12 19:52:10 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,39 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+  T data[size];
+ public:
+  int top;
+  TStack() : top(0) {}
+
+  void push(const T &item) {
+    if (top <= size - 1) {
+      data[top++] = item;
+    } else {
+      throw std::string("Stack overflow");
+    }
+  }
+
+  T pop() {
+    if (top > 0) {
+      return data[--top];
+    } else {
+      throw std::string("Stack is empty");
+    }
+  }
+
+  T get() const {
+    if (top > 0) {
+      return data[top - 1];
+    } else {
+      throw std::string("Empty");
+    }
+  }
+
+  bool isEmpty() const {
+    return top == 0;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
